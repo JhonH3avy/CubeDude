13cc5050fed1bb8b0e92898ec2651da1d2f2db0a
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2019-08-13 13:45:43 -0500

    fixed cubedude movement

--- Scripts/Cubedude.gd
+++ Scripts/Cubedude.gd
@@ -6,7 +6,15 @@ const UP = Vector3(0,1,0)
 const GRAVITY = -5
 const EPSILON = 0.1
 
+export var player_number = "1"
+
 export var speed = 10
+
+export var color_material : Material
+
+
+func _ready():
+	$"Armature/Skeleton/Cube Dube".set_surface_material(0,color_material)
 
 
 func _physics_process(delta):
@@ -30,19 +38,19 @@ func animate():
 
 func face_forward():
 	if not motion.x == 0 or not motion.z == 0:
-		look_at(Vector3(-motion.x, 0, -motion.z), UP)
+		look_at(Vector3(translation.x - motion.x, 0, translation.z - motion.z), UP)
 
 
 func move():
-	if Input.is_action_pressed("left_1") and not Input.is_action_pressed("right_1"):
+	if Input.is_action_pressed("left_" + player_number) and not Input.is_action_pressed("right_" + player_number):
 		motion.x = -1
-	elif Input.is_action_pressed("right_1") and not Input.is_action_pressed("left_1"):
+	elif Input.is_action_pressed("right_" + player_number) and not Input.is_action_pressed("left_" + player_number):
 		motion.x = 1
 	else:
 		motion.x = 0
-	if Input.is_action_pressed("down_1") and not Input.is_action_pressed("up_1"):
+	if Input.is_action_pressed("down_" + player_number) and not Input.is_action_pressed("up_" + player_number):
 		motion.z = 1
-	elif Input.is_action_pressed("up_1") and not Input.is_action_pressed("down_1"):
+	elif Input.is_action_pressed("up_" + player_number) and not Input.is_action_pressed("down_" + player_number):
 		motion.z = -1
 	else:
 		motion.z = 0
